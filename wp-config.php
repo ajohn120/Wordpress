99325bf81df8b59f65efa8388fdf2860e05d70a7
ajohn120 <90159136+ajohn120@users.noreply.github.com>
2022-12-03 11:30:22 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'fullsail');
+define('DB_USER', 'wpuser');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Fullsail1');
+define('DB_PASSWORD', 'fulsail1');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
